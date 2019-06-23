7ea1756c51f920e96d74d3886904dedeeb180bc0
Juraj <duro24@gmail.com>
2019-06-23 16:47:25 +0200

    Determinant and Cramer's rule working

--- elipsoid/elipsoid.c
+++ elipsoid/elipsoid.c
@@ -2,6 +2,98 @@
 #include <stdio.h>
 #include <stdlib.h>
  
+ 
+float * submatrix(float *M, int n, int k){
+    float * subM = (float *) malloc((n-1) * (n-1) * sizeof (float));
+    
+    int jj = 0;
+    for (int i = 1; i<n; i++) {
+        jj = 0;
+        for (int j = 0; j<n; j++) {
+            if (j == k) {
+                jj = 1;
+                continue;
+            }
+            subM[(i-1) * (n-1) + (j - jj)] = M[i*n + j];
+        }
+    }
+    
+    return subM;
+}
+ 
+ 
+float determinant(float *M, int n){
+        
+    printf("--> Det n=%d\n", n);
+    if (n < 1){
+        printf("This shold not be possible!\n");
+        return 1;
+    }
+    
+    if (n == 1) {
+        printf("result=%f\n", M[0]);
+        return M[0];
+    }
+    
+    float result = 0;
+    float sign = -1.0;
+    for (int i = 0; i < n; i++) {
+        sign *= -1.0;
+        float * subM = submatrix(M, n, i);
+        result += sign * M[i] * determinant( subM, n-1);
+        free(subM);
+    }
+    
+    printf("<-- Det n=%d, result=%f\n", n, result);
+    return result;
+}
+
+
+/**
+ * Solving linear system using Cramer's rule
+ * 
+ * @param M matrix n x n
+ * @param b right hand side 1 x n
+ * @param n size of the matrix 
+ * @param x solution
+ */
+int solve_lin_system(float * M, float * b, int n, float * x){
+    
+    if (n<1) return -1;
+    
+    if (n == 1) {
+        if (M[0] == 0) {
+            return -1;
+        }
+        x[0] = b[0] / M[0];
+        return 0;
+    }
+    
+    float detM = determinant(M, n);
+    if (detM == 0) {
+        return -1;
+    }
+    
+    float * v = (float *) malloc (n * sizeof(float));
+    
+    for (int i = 0; i<n; ++i){
+        for (int j =0; j < n; ++j){
+            v[j] = M[j * n + i];
+            M[j * n + i] = b[j];
+        }
+        float detMi = determinant(M, n);
+
+        x[i] = detMi / detM;
+        
+        for (int j =0; j < n; ++j){
+            M[j * n + i] = v[j];
+        }
+    }
+    
+    free(v);
+    return 0;
+}
+ 
 /*
 D = [ x .* x + y .* y - 2 * z .* z, ...
         x .* x + z .* z - 2 * y .* y, ...
@@ -11,7 +103,7 @@ D = [ x .* x + y .* y - 2 * z .* z, ...
         2 * x, ...
         2 * y, ...
         2 * z, ...
-        1 + 0 * x ];  % ndatapoints x 9 ellipsoid parameters
+        1 + 0 * x ];  % ndatapoints x 9 ellipsoid parameterspo
         
         % solve the normal system of equations
 d2 = x .* x + y .* y + z .* z; % the RHS of the llsq problem (y's)
@@ -73,19 +165,84 @@ int fit_elipsoid(float *x, float *y, float *z, int n, float *res) {
     }
     
     
-    
     float sum = 0;
     for (int i = 0; i < n; ++i) {
         sum += D[9*i];
     }
     
+    free(D);
+    
     res[0] = sum;
     return 0;
 }
 
- 
+
+void testSubM(float * M2) {
+    
+    for (int i = 0; i<3; ++i){
+        float * subM = submatrix(M2, 3, i);
+        printf("\nSubmatrix %d of M2 = \n %f\t%f\n %f\t%f\n", i, subM[0], subM[1], subM[2], subM[3]);
+        printf("SubSubmatrix: \n");
+        for (int j = 0; j<2; ++j){    
+            float * subsubM = submatrix(subM, 2, j);
+            printf("\t%f\n", subsubM[0]);
+            free(subsubM);
+        }
+        free(subM);
+    }
+    
+}
+
+void testDet(){
+        float M[9] = {
+        1,0,0,
+        0,1,0,
+        0,0,1
+    };
+    
+    float M2[9] = {
+        11,12,13,
+        21,22,23,
+        31,32,33
+    };
+    
+//     testSubM(M2);    
+//     testSubM(M);
+    
+    printf("Determinat of I = %f\n", determinant(M, 3));
+    printf("Determinat of M2 = %f\n", determinant(M2, 3));
+}
+
+void testLinSolv(){
+    /***************************
+     * Matlab solution:
+     * 
+     * ans =
+     *   
+     *    0.7727
+     *    0.4545
+     *   -0.2273
+     */
+    float M[9] = {
+        1, 2, 3,
+        7,-7, 1,
+        3, 2, 1
+    };
+    
+    float b[3] = {1,2,3};
+    
+    float v[3] = {0};
+    
+    int ret = solve_lin_system(M, b, 3, v);
+    
+    printf("Solution to the system is: \n\t%f \nx = \t%f \n\t%f\n\n", v[0], v[1], v[2]);
+    
+}
+
 int main(){
      
+    testLinSolv();
+    /*
     float x[10] = {1,2,3,4,5,6,7,8,9,0};
     float y[10] = {0,1,2,3,4,5,6,7,8,9};
     float z[10] = {1,2,3,4,5,6,7,8,9,10};
@@ -96,5 +253,7 @@ int main(){
     printf("Hello world!\n");
     printf("The result is: %f, %f, %f, return code: %d\n", res[0], res[1], res[2], ret);
     return 0;
+    */
  }
+ 
  
