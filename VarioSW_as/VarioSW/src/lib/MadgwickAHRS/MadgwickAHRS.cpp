cf2d756881f2e72fb46419e9c59067e1106db653
Juraj Palenik <duro24@gmail.com>
2020-03-06 01:31:45 +0100

    Compass fix

--- VarioSW_as/VarioSW/src/lib/MadgwickAHRS/MadgwickAHRS.cpp
+++ VarioSW_as/VarioSW/src/lib/MadgwickAHRS/MadgwickAHRS.cpp
@@ -16,7 +16,6 @@
 // Header files
 
 #include "MadgwickAHRS.h"
-#include <math.h>
 #include "Arduino.h"
 
 //---------------------------------------------------------------------------------------------------
@@ -228,8 +227,8 @@ float Madgwick::invSqrt(float x) {
 
 void Madgwick::computeAngles(){
 	roll = (atan2f(q0*q1 + q2*q3, 0.5f - q1*q1 - q2*q2));
-	pitch = (asinf(-2.0f * (q1*q3 - q0*q2)));
-	yaw = (atan2f(q1*q2 + q0*q3, 0.5f - q2*q2 - q3*q3));
+	pitch = (asinf(-2.0f * (q1*q3 - q0*q2)));				// 
+	yaw = (atan2f(q1*q2 + q0*q3, 0.5f - q2*q2 - q3*q3));  // x axis along long vario edge
 }
 
 //====================================================================================================
